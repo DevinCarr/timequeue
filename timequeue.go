19ac34e6109b0c54a517b2c8ccf1cc4117f947c0
devincarr <devin.carr0@gmail.com>
2016-10-01 16:10:42 -0500

    Added Code

---
+++ timequeue.go
@@ -0,0 +1,63 @@
+package timequeue
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Store struct {
+	expiry  time.Duration
+	id      uint64
+	address string
+}
+
+type TimeQueue struct {
+	count uint64
+}
+
+// NewTimeQueue creates a new TimeQueue object
+func NewTimeQueue() *TimeQueue {
+	return &TimeQueue{
+		count: 0,
+	}
+}
+
+// wait will wait for the duration of the store and then call the
+// provided url.
+func wait(tq *TimeQueue, s *Store) {
+	time.Sleep(s.expiry)
+	tq.Done()
+	resp, err := http.PostForm(s.address,
+		url.Values{"value": {"123"}})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Body)
+}
+
+// Push puts a new item in the queue with a time d in hours
+func (tq *TimeQueue) Push(d float64, id uint64) {
+	tq.count += 1
+	fmt.Println("Put new item")
+	go wait(tq, &Store{
+		expiry: time.Duration(d) * time.Hour,
+		id:     id,
+	})
+}
+
+// Done will remove the item in the queue and return the count of
+// remaining items.
+func (tq *TimeQueue) Done() uint64 {
+	tq.count -= 1
+	return tq.count
+}
+
+// Count returns the current amount of items in the queue
+func (tq *TimeQueue) Count() uint64 {
+	return tq.count
+}
---
+++ timequeue_test.go
@@ -0,0 +1,22 @@
+package timequeue
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	tq := NewTimeQueue()
+
+	tq.Push(0.1, 123)
+
+	if tq.Count() != 1 {
+		t.Error("Count was not 1 after Push")
+	}
+
+	for tq.Count() > 0 {
+	}
+
+	if tq.Count() != 0 {
+		t.Error("timequeue should be empty")
+	}
+}
